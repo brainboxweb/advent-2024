59f7e3479e03197768443b0a67008afb556f2261
Gary Straughan <gary.straughan@brainboxweb.co.uk>
2025-03-04 09:19:06 +0000

    Day 2

---
+++ day2/challenge.go
@@ -0,0 +1,117 @@
+package day2
+
+import (
+	"strconv"
+	"strings"
+)
+
+func ChallengeOne(data []string) int {
+	reports := parse(data)
+	count := 0
+	for _, report := range reports {
+		if isSafe(report) {
+			count++
+		}
+	}
+
+	return count
+}
+
+//revive:disable:cognitive-complexity
+func ChallengeTwo(data []string) int {
+	reports := parse(data)
+	count := 0
+OUTER:
+	for _, report := range reports {
+		if isSafe(report) {
+			count++
+			continue
+		}
+		for i := range len(report) {
+			rep := dropElement(report, i)
+			if isSafe(rep) {
+				count++
+				continue OUTER
+			}
+		}
+	}
+
+	return count
+}
+
+//revive:enable:cognitive-complexity
+
+func dropElement(report []int, element int) []int {
+	ret := []int{}
+	for i := range len(report) {
+		if i == element {
+			continue
+		}
+		ret = append(ret, report[i])
+	}
+	return ret
+}
+
+func parse(data []string) [][]int {
+	ret := [][]int{}
+	for _, line := range data {
+		rep := []int{}
+		parts := strings.Split(line, " ")
+		for _, i := range parts {
+			p1, _ := strconv.Atoi(i)
+			rep = append(rep, p1)
+		}
+		ret = append(ret, rep)
+	}
+
+	return ret
+}
+
+func isSafe(report []int) bool {
+	if !areSmallSteps(report) {
+		return false
+	}
+
+	return isSameDirection(report)
+}
+
+func areSmallSteps(report []int) bool {
+	for i := 1; i < len(report); i++ {
+		diff := report[i] - report[i-1]
+		if diff > 3 {
+			return false
+		}
+		if diff < -3 {
+			return false
+		}
+	}
+
+	return true
+}
+
+//revive:disable:cognitive-complexity
+func isSameDirection(report []int) bool {
+	direction := 0
+	for i := 1; i < len(report); i++ {
+		diff := report[i] - report[i-1]
+		if diff == 0 {
+			return false
+		}
+		if diff > 0 {
+			if direction == -1 {
+				return false
+			}
+			direction = 1
+		}
+		if diff < 0 {
+			if direction == 1 {
+				return false
+			}
+			direction = -1
+		}
+	}
+
+	return true
+}
+
+//revive:enable:cognitive-complexity
---
+++ day2/challenge_test.go
@@ -0,0 +1,58 @@
+package day2_test
+
+import (
+	"testing"
+
+	"github.com/brainboxweb/advent-2024/day2"
+	"github.com/brainboxweb/advent-2024/helpers"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const testDataPath = "../data/"
+
+func TestDay2(t *testing.T) {
+	tests := []struct {
+		dataFile string
+		expected int
+	}{
+		{
+			"day2_test.txt",
+			2,
+		},
+		{
+			"day2.txt",
+			559,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.dataFile, func(t *testing.T) {
+			dataSet := helpers.GetDataString(testDataPath + tt.dataFile)
+			result := day2.ChallengeOne(dataSet)
+			assert.Equal(t, tt.expected, result)
+		})
+	}
+}
+
+func TestDay2Part2(t *testing.T) {
+	tests := []struct {
+		dataFile string
+		expected int
+	}{
+		{
+			"day2_test.txt",
+			4,
+		},
+		{
+			"day2.txt",
+			601,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.dataFile, func(t *testing.T) {
+			dataSet := helpers.GetDataString(testDataPath + tt.dataFile)
+			result := day2.ChallengeTwo(dataSet)
+			assert.Equal(t, tt.expected, result)
+		})
+	}
+}
